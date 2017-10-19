2dc7d3cb6744ce125c3526dd27c8c5a96166a8ce
wjdgns011001 <wjdgns011001@naver.com>
2017-10-19 00:31:39 +0000

    17.10.19

--- racoon/message.php
+++ racoon/message.php
@@ -2,57 +2,37 @@
 
 $data = json_decode(file_get_contents('php://input'), true);
 
+$today=getdate();
 $content = $data["content"];
-if($content == "안녕"){ 
+if($content == "대화"||substr($content,0,6) == "하이"||substr($content,0,3) == "안"){ 
+echo <<< EOD
+{
+    "message": {
+"text": "안녕하세요 주인님" }
+}
+EOD;
+}
 
+else if(substr($content,0,6) == "반가"||substr($content,0,6) == "방가"){
 echo <<< EOD
 
-{
-
     "message": {
-
-        "text": "안녕하세요 주인님"
-
+        "text" : "반갑습니다 ^^"
     }
-
-  
+}
+EOD;
+}
+else if ($content="날짜"){
+echo <<< EOD
+{
+    "message": {
+      "text" :"오늘은 $today[year] 년 $today[mon] 월 $today[mday] 일 $today[weekday] $today[hours]시 $today[minutes]분 $today[seconds]초"
+    }
 }
 
 EOD;
-
 }
 
-else if($content == "반가워"){
 
-echo <<< EOD
-
-{
-
-    "message": {
-
-        "text" : "저도 반가워요."
-    }
-
-}
-
-EOD;
-
-}
-else{
-
-echo <<< EOD
-
-{
-
-    "message": {
-
-        "text" : substr($content,0,2);
-    }
-
-}
-
-EOD;
-
-}
 
 ?>
