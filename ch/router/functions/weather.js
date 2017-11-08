ad584cc03f12377d7eb3c01e068d65f4ce6b5de6
chlee1001 <chlee1001@naver.com>
2017-11-09 01:17:30 +0900

    airpollution update

--- ch/router/functions/weather.js
+++ ch/router/functions/weather.js
@@ -1,6 +1,7 @@
 /**
  * Created by chlee1001 on 2017-10-31.
  */
+
 module.exports.weather = function (callback) {
 	var request = require('request');
 	require('date-utils');
@@ -16,41 +17,51 @@ module.exports.weather = function (callback) {
 			'appkey': token
 		}
 	};
+	
+	var result2 = '';
+	var air = require('./airpollution');
+	
+	air.airpollution(function (result2) {
+		//console.log(result2);
 
-	request.get(options, function (error, response, body) {
-		//request 성공했으면..
-		if (!error && response.statusCode == 200) {
-			//json 파싱
-			var objBody = JSON.parse(response.body);
-			//필요한 부분만 추출
+		request.get(options, function (error, response, body) {
+			//request 성공했으면..
+			if (!error && response.statusCode == 200) {
 
-			var sky = objBody.weather.hourly[0].sky.name;
-			var temperature = objBody.weather.hourly[0].temperature;
-			var humidity = objBody.weather.hourly[0].humidity;
+				//json 파싱
+				var objBody = JSON.parse(response.body);
+				//필요한 부분만 추출
 
-			result = sky + '\n현재 기온: ' + temperature.tc + '℃\n최고 기온: ' + temperature.tmax
-				 + '℃\n최저 기온: ' + temperature.tmin + '℃\n습도: ' + humidity + '%';
+				var sky = objBody.weather.hourly[0].sky.name;
+				var temperature = objBody.weather.hourly[0].temperature;
+				var humidity = objBody.weather.hourly[0].humidity;
 
-			//카톡으로 번역된 메시지를 전송하기 위한 메시지
-			let message = {
-				"message": {
-					"text": '현재 ' + today + '의 날씨 정보(해)(구름)(비)\n\n' + result
-				},
-				"keyboard": {
-					"type": "buttons",
-					"buttons": [
-						"학식",
-						"식당추천",
-						"날씨",
-						"번역기",
-						"사진",
-						"처음으로"
-					]
-				}
-			};
-			//카톡에 메시지 전송
-			return callback(message);
+				result = '현재 ' + today + '의 날씨 정보(해)(구름)(비)\n\n' + sky
+					 + '\n현재 기온: ' + temperature.tc + '℃\n최고 기온: ' + temperature.tmax
+					 + '℃\n최저 기온: ' + temperature.tmin + '℃\n습도: ' + humidity + '%\n미세먼지\n\b' + result2;
 
-		}
-	});
+				//카톡으로 번역된 메시지를 전송하기 위한 메시지
+				let message = {
+					"message": {
+						"text": result
+					},
+					"keyboard": {
+						"type": "buttons",
+						"buttons": [
+							"학식",
+							"식당추천",
+							"날씨",
+							"번역기",
+							"사진",
+							"처음으로"
+						]
+					}
+				};
+				//카톡에 메시지 전송
+				return callback(message);
+
+			}
+		});
+	})
+
 }
