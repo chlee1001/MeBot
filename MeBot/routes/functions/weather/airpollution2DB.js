48a8282bfbd55d69da5fbd4b6f3cc9e5171b3314
chlee1001 <chlee1001@naver.com>
2017-11-14 12:14:13 +0900

    change airpollutions locate

--- MeBot/routes/functions/weather/airpollution2DB.js
+++ MeBot/routes/functions/weather/airpollution2DB.js
@@ -15,10 +15,8 @@ module.exports = function (app, mysql, connection) {
 		database: "computerNetwork"
 	};
 
-	var connection;
-
 	function handleDisconnect() {
-		connection = mysql.createConnection(db_config); // Recreate the connection, since
+		var connection = mysql.createConnection(db_config); // Recreate the connection, since
 		// the old one cannot be reused.
 
 		connection.connect(function (err) { // The server is either down
@@ -34,7 +32,7 @@ module.exports = function (app, mysql, connection) {
 		// If you're also serving http, display a 503 error.
 		connection.on('error', function (err) {
 			console.log('db error', err);
-			if (err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+			if (err.code == 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
 				handleDisconnect(); // lost due to either server restart, or a
 			} else { // connnection idle timeout (the wait_timeout
 				throw err; // server variable configures this)
@@ -65,7 +63,7 @@ function updateDB(connection) {
 			parser.parseString(xml, function (err, result) {
 
 				//var objBody = JSON.parse(strBody);
-				result = (result.response.body[0].items[0].item[11]);
+				result = (result.response.body[0].items[0].item[12]);
 
 				// table 초기화
 				var deleteQuery = connection.query(
